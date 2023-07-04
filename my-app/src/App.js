d3ee2b8123710f5da1bb8c84517354acbd44d9ea
AndyR8219 <andreymail1982@gmail.com>
2023-07-04 22:20:31 +0300

    without jsx

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { createElement } from 'react';
+
 const year = new Date().getFullYear();
+const $imgHeader = createElement('img', { class: 'App-logo', alt: 'logo', src: logo });
+const $pHeader = createElement(
+	'p',
+	null,
+	'Edit',
+	createElement('code', null, 'src/App.js'),
+	'and save to reload',
+);
+const $aHeader = createElement(
+	'a',
+	{
+		class: 'App-link',
+		href: 'https://reactjs.org',
+		target: '_blank',
+		rel: 'noopener noreferrer',
+	},
+	'Learn React',
+);
+const $pYear = createElement('p', null, year);
+const $header = createElement(
+	'header',
+	{ class: 'App-header' },
+	$imgHeader,
+	$pHeader,
+	$aHeader,
+	$pYear,
+);
+const $blockApp = createElement('div', { class: 'App' }, $header);
 
 export function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>{year}</p>
-			</header>
-		</div>
-	);
+	return $blockApp;
 }
